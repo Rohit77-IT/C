110076b4534e8c6edf62fc3a98cb3ce79e4f9589
Rohit77-IT <dey71393@gmail.com>
2026-01-04 11:49:02 +0530

    sum of the series

---
+++ Sum_of_series.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    float sum = 0.0;
+    printf("Enter the number of terms: ");
+    scanf("%d",&n);
+    for(int i=1;i<n;i++)
+    {
+        sum += 1.0/i;
+    }
+    printf("Sum of the series is : %.2f\n",sum);
+    return 0;
+}
